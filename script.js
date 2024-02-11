39884c709c9940e60cf172f9ae6a9cc94c4c35de
Raouf404 <roaufdz222@gmail.com>
2024-02-11 15:59:32 +0100

    Added the input system

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ let choices = ['Rock', 'Paper', 'Scissors'];
 function getCompmuterChoice() {
   return Math.floor(Math.random() * 3);
 }
+
+function playerSelection() {
+  let selection = prompt('Rock, Paper or Scissors');
+  if (selection.toLowerCase === 'rock' ||  selection === '0') {
+    return 0;
+  } else if (selection.toLowerCase === 'paper' ||  selection === '1') {
+    return 1;
+  } else if (selection.toLowerCase === 'scissors' ||  selection === '2') {
+    return 2;
+  } else {
+    alert('Invalid input! Please enter Rock, Paper or Scissors only.');
+    return false;
+  }
+}
